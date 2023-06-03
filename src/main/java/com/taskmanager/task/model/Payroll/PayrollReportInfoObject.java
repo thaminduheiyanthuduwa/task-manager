c915b9beb547b69661e7284ecf3c886cc77d13ba
thaminduheiyanthuduwa <thaminduanu2@gmail.com>
2023-06-03 13:11:39 +0530

    Add changes

---
+++ src/main/java/com/taskmanager/task/model/Payroll/PayrollReportInfoObject.java
@@ -0,0 +1,139 @@
+package com.taskmanager.task.model.Payroll;
+
+import com.taskmanager.task.entity.PayrollEntityDetails;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class PayrollReportInfoObject {
+
+    private String id;
+
+    private String name;
+
+    private String epfNo;
+
+    private String designation;
+
+    private String date;
+
+    private String basicSalary;
+
+    private String basic_salary;
+
+    private String Budgetary_Allowance;
+
+    private String Academic_Incentive_Payment;
+
+    private String Attendance_Incentive_Payment;
+
+    private String Crisis_Allowance;
+
+    private String Dean_Incentive_Payment;
+
+    private String Fixed_OT;
+
+    private String Fixed_Service_Charges;
+
+    private String HOD_Incentive_Payment;
+
+    private String Insurance_Premium;
+
+    private String Leadership_Incentive_Payment;
+
+    private String Neatness_Incentive_Payment;
+
+    private String Performance_Incentive_Payment;
+
+    private String Rent_Reimbursement;
+
+    private String Research_Incentive_Payment;
+
+    private String Service_Charges;
+
+    private String Travelling_Allowance;
+
+    private String MSc_Qualification_Incentive;
+
+    private String Crisis_Allowances_2022;
+
+    private String DVC_Incentive_Allowance;
+
+    private String MSC_Phd_Incentive;
+
+    private String Performance_2022;
+
+    private String Travelling_Allo_22;
+
+
+
+
+    private String noPay;
+
+    private String setFinalizedBasicSalary;
+
+    private String morningLate;
+
+    private String otAmount;
+
+    private String grossSalary;
+
+    private String salaryForEPF_ETF;
+
+    private String housingAllowance;
+
+    private String travellingAllowance;
+
+    private String hodIncentivePayment;
+
+    private String deanIncentivePmt;
+
+    private String incentiveForResearch;
+
+    private String performanceIncentive;
+
+    private String quarterPerformanceIncentive;
+
+    private String taskIncentivePayment;
+
+    private String reimbursement;
+
+    private String mscIncentivePayment;
+
+    private String dvcIncentivePayment;
+
+    private String crisisIncentivePayment;
+
+    private String totalSalary;
+
+    private String payee;
+
+    private String epf8;
+
+    private String swa;
+
+    private String lateAttendance;
+
+    private String taskDeduction;
+
+    private String penalty;
+
+    private String loan;
+
+    private String salaryAdvance;
+
+    private String totalDeduction;
+
+    private String netSalary;
+
+    private String epf12;
+
+    private String etf3;
+
+    private String total;
+
+
+}
