6159ec3e055e8bae2c07b4623fd7a79d327ed497
Administrator <thaminduanu2@gmail.com>
2023-10-22 11:01:35 +0530

    Add changes

--- src/main/java/com/taskmanager/task/service/impl/AttendanceManagerImpl.java
+++ src/main/java/com/taskmanager/task/service/impl/AttendanceManagerImpl.java
@@ -1084,7 +1084,7 @@ public class AttendanceManagerImpl implements AttendanceManager {
     @Override
     public ResponseList updateLeaveWithAttendance() throws ParseException {
 
-        List<LeaveEntity> listObj = leaveRepository.getLeaveByDate("2023-09-01");
+        List<LeaveEntity> listObj = leaveRepository.getLeaveByDate("2023-11-01");
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
         SimpleDateFormat convertDateToDateOnly = new SimpleDateFormat("yyyy-MM-dd");
@@ -1139,7 +1139,7 @@ public class AttendanceManagerImpl implements AttendanceManager {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         SimpleDateFormat convertDateToDateOnly = new SimpleDateFormat("yyyy-MM-dd");
 
-        List<AttendanceEntity> listObj = attendanceRepository.findByDateRange("2023-08-01", "2023-10-01");
+        List<AttendanceEntity> listObj = attendanceRepository.findByDateRange("2023-10-01", "2023-12-01");
 
         List<AttendanceEntity> newList = new ArrayList<>();
 
